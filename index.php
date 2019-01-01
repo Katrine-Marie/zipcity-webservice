0a186bec51448bc700367ad770e092744926d334
fjordscene@gmail.com <fjordscene@gmail.com>
2019-01-01 12:29:19 +0100

    Added the GET routes

--- index.php
+++ index.php
@@ -10,7 +10,34 @@ $httpMethod = $_SERVER['REQUEST_METHOD'];
 $uri = explode("/", $_SERVER['REQUEST_URI']);
 
 if($httpMethod == 'GET'){
+  if(isset($uri[2]) && $uri[1] == 'zip'){
+    // Is this a valid Danish zipcode?
+    if(preg_match('/^(\d{3,4})?$/', $uri[2])){
+      header('Content-type: application/json');
 
+      $city = $dbCon->getCityByZipCode($uri[2]);
+      echo json_encode($city);
+    } else {
+      echo 'ERROR WRONG ZIPCODE FORMAT!';
+    }
+  }else if(isset($uri[2]) && $uri[1] == 'city'){
+    // Is this a valid Danish zipcode?
+    if(preg_match('/^(\d{3,4})?$/', $uri[2])){
+      header('Content-type: application/json');
+
+      $city = $dbCon->getZipCodeByCity(urldecode($uri[2]));
+      echo json_encode($city);
+    } else {
+      echo 'ERROR WRONG CITYNAME!';
+    }
+  }else if($uri[1] == 'zip'){
+		header('Content-type: application/json');
+
+    $zip = $dbCon->getAllCities();
+    echo json_encode($zip);
+  }else {
+    echo 'WRONG PATH';
+  }
 }else {
   header('HTTP/1.0 403 Forbidden');
 }
